42e776ef23dc6f41aa16602fdd3d488c9b9613c6
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-02 04:12:55 -0400

    fixed typo

--- src/db/migrations/20210702040730_createCriticsTable.js
+++ src/db/migrations/20210702040730_createCriticsTable.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     table.increments("critic_id").primary();
     table.string("preferred_name");
     table.string("surname");
-    table.string("organization_nam");
+    table.string("organization_name");
   });
 };
 
